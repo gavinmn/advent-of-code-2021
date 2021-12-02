b66905a75bfe1383e865817dc757b5c0c439b55f
Gavin Nelson <gavin@nelson.co>
2021-12-02 12:16:29 -0800

    day 2 part 2

--- day2/day2.js
+++ day2/day2.js
@@ -22,4 +22,24 @@ function part1(position) {
 	return result
 }
 
-console.log(part1(position))
+function part2(position) {
+	let forward = 0
+	let aim = 0
+	let depth = 0
+	for (let i = 0; i < position.length; i++) {
+		let direction = position[i].split(" ")[0]
+		let ammount = parseInt(position[i].split(" ")[1])
+		if (direction == "forward") {
+			forward += ammount
+			depth += aim * ammount
+		} else if (direction == "down") {
+			aim += ammount
+		} else if (direction == "up") {
+			aim -= ammount
+		}
+	}
+	let result = forward * depth
+	return result
+}
+
+console.log(part2(position))
